da123cd20554b4112e021056cd3db9a59f0e6fb6
EC2 Default User <ec2-user@ip-172-31-26-84.us-east-2.compute.internal>
2019-09-19 06:11:32 +0000

    Delete age

--- db/migrate/20190919054809_add_age_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddAgeToMessages < ActiveRecord::Migration[5.1]
-  def change
-    add_column :messages, :age, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20190919054809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "age"
   end
 
 end
